10370cfd2b35ed8e6d79d1c30c0c016053cec84d
Rosemond Osei <rosemondosei045@gmail.com>
2025-11-15 13:29:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1760621235-adf38",
+  "appId": "1:871771437538:web:724d8eeeb9713773caa1bb",
+  "apiKey": "AIzaSyC61AKjOlXXNqG0gD-luojOnCM8W7W6oZE",
+  "authDomain": "studio-1760621235-adf38.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "871771437538"
+};
